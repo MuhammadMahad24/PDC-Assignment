9cbd6399d13e72f5660d2c28c5c535a10f193664
MuhammadMahad24 <mahadxhahbaz@example.com>
2025-03-07 22:38:10 +0500

    Time Functions added.

--- Sequential Implementation.c
+++ Sequential Implementation.c
@@ -20,13 +20,22 @@ int main()
     long long sum;
 
     //Initialize array with random-values
-    
+    srand(time(NULL));
     for(int i=0;i<N;i++)
     {
         arr[i]=rand() % 10;
     }
+
+clock_t start = clock();
+arraySumSequential(arr, &sum);
+clock_t end = clock();
+
+double time_taken=((double)(end - start)) / CLOCKS_PER_SEC * 1000;
+printf("Sequential Sum: %lld\n", sum);
+printf("Sequential Execution Time: %.2f ms\n", time_taken);
+
+return 0;
+
 }
-
-
 
 
