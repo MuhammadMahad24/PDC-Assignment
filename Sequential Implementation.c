ce889492d2fbd9ca01ef10ce065ed00e1df5dec5
MuhammadMahad24 <mahadxhahbaz@example.com>
2025-03-07 22:24:23 +0500

    Array sum Function Added

---
+++ Sequential Implementation.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 1000000 //Array-size define directive
+
+//Array-sum Function
+void arraySumSequential(int arr[N],long long *sum)
+{
+    *sum=0;
+    for(int i=0;i<N;i++)
+    {
+        *sum+=arr[i];
+    }
+}
