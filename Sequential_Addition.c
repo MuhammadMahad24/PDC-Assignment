73c897e2b1dc2af63fb8910cab73f7f9b51e1c03
MuhammadMahad24 <mahadxhahbaz@example.com>
2025-03-10 23:08:00 +0500

    Addition functions matrix is initialized and time calculation added.

--- Sequential_Addition.c
+++ Sequential_Addition.c
@@ -19,3 +19,28 @@ void printMatrix(int M[N][N]) {
         printf("\n");
     }
 }
+int main() {
+    static int A[N][N], B[N][N], C[N][N];
+
+    // Initialize matrices with random values
+    srand(time(NULL));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i][j] = rand() % 10;
+            B[i][j] = rand() % 10;
+        }
+    }
+    // Sequential execution
+    clock_t start = clock();
+    matrixAdditionSequential(A, B, C);
+    clock_t end = clock();
+
+     // Print Resultant Matrix
+     printf("\nResultant Matrix C (Sum):\n");
+     printMatrix(C);
+
+    double time_taken = ((double)(end - start) / CLOCKS_PER_SEC) * 1000;
+    printf("Sequential Execution Time: %.2f ms\n", time_taken);
+
+    return 0;
+} 
