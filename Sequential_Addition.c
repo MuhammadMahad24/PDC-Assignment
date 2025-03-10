190a9f229c0c1fd31e4140de9ca8bebec2262b3f
MuhammadMahad24 <mahadxhahbaz@example.com>
2025-03-10 23:07:09 +0500

    Addition functions added.

---
+++ Sequential_Addition.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 1000  // Matrix size (N x N)
+
+void matrixAdditionSequential(int A[N][N], int B[N][N], int C[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+} 
+void printMatrix(int M[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", M[i][j]);
+        }
+        printf("\n");
+    }
+}
